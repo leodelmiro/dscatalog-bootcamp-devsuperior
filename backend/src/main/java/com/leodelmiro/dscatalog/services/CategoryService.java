71810f0c5b7fb709318da0642c9b0b82d7424d7f
Leonardo Delmiro <leodelmiroms@gmail.com>
2020-09-29 22:51:25 -0300

    H2 database

---
+++ backend/src/main/java/com/leodelmiro/dscatalog/services/CategoryService.java
@@ -0,0 +1,19 @@
+package com.leodelmiro.dscatalog.services;
+
+import com.leodelmiro.dscatalog.entities.Category;
+import com.leodelmiro.dscatalog.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<Category> findAll(){
+        return categoryRepository.findAll();
+    }
+}
