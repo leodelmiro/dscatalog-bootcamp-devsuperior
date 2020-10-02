476f77e8147e540282d84bac01adbf8c2edff82e
Leonardo Delmiro <leodelmiroms@gmail.com>
2020-10-02 18:41:47 -0300

    Product Repository, Service, Resource, GET Ok

--- backend/src/main/java/com/leodelmiro/dscatalog/dto/ProductDTO.java
+++ backend/src/main/java/com/leodelmiro/dscatalog/dto/ProductDTO.java
@@ -1,46 +1,51 @@
-package com.leodelmiro.dscatalog.entities;
+package com.leodelmiro.dscatalog.dto;
 
-import javax.persistence.*;
+import com.leodelmiro.dscatalog.entities.Category;
+import com.leodelmiro.dscatalog.entities.Product;
+
 import java.io.Serializable;
 import java.time.Instant;
-import java.util.HashSet;
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
-@Entity
-@Table(name = "tb_product")
-public class Product implements Serializable {
+public class ProductDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-
-    @Column(columnDefinition = "TEXT")
     private String description;
     private Double price;
     private String imgUrl;
-
-    @Column (columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant date;
 
-    @ManyToMany
-    @JoinTable(name = "tb_product_category",
-            joinColumns = @JoinColumn(name = "product_id"),
-            inverseJoinColumns = @JoinColumn(name = "category_id"))
-    private Set<Category> categories = new HashSet<>();
+    private List<CategoryDTO> categories = new ArrayList<>();
 
-    public Product(){
+    public ProductDTO() {
 
     }
 
-    public Product(Long id, String name, String description, String imgUrl, Instant date) {
+    public ProductDTO(Long id, String name, String description, Double price, String imgUrl, Instant date) {
         this.id = id;
         this.name = name;
         this.description = description;
+        this.price = price;
         this.imgUrl = imgUrl;
         this.date = date;
+    }
+
+    public ProductDTO(Product entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+        this.description = entity.getDescription();
+        this.price = entity.getPrice();
+        this.imgUrl = entity.getImgUrl();
+        this.date = entity.getDate();
+    }
+
+    public ProductDTO(Product entity, Set<Category> categories) {
+        this(entity);
+        categories.forEach(cat -> this.categories.add(new CategoryDTO(cat)));
     }
 
     public Long getId() {
@@ -91,20 +96,7 @@ public class Product implements Serializable {
         this.date = date;
     }
 
-    public Set<Category> getCategories() {
+    public List<CategoryDTO> getCategories() {
         return categories;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Product product = (Product) o;
-        return Objects.equals(id, product.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
     }
 }
---
+++ backend/src/main/java/com/leodelmiro/dscatalog/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.leodelmiro.dscatalog.repositories;
+
+import com.leodelmiro.dscatalog.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
