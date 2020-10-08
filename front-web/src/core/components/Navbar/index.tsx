e73ebf14647a9f5ec882724de4cbdae517dec993
Leonardo Delmiro <leodelmiroms@gmail.com>
2020-10-08 18:57:15 -0300

    ProductDetails finished

--- front-web/src/core/components/Navbar/index.tsx
+++ front-web/src/core/components/Navbar/index.tsx
@@ -18,7 +18,7 @@ const Navbar = () => (
                         </NavLink>
                     </li>
                     <li>
-                        <NavLink to="/catalog" activeClassName="active">
+                        <NavLink to="/products" activeClassName="active">
                             CATALÓGO
                         </NavLink>
                     </li>
