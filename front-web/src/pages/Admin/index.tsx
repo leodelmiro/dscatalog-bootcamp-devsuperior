e061fcc1f2df9a5b4e15b0fcf480ffc6b1d2595b
Leonardo Delmiro <leodelmiroms@gmail.com>
2020-10-16 16:22:59 -0300

    Admin Routes Base Structure finished

--- front-web/src/pages/Admin/index.tsx
+++ front-web/src/pages/Admin/index.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Products from './components/Products';
+import './styles.scss';
 
 const Admin = () => (
-    <div>
+    <div className="admin-container">
         <Navbar/>
+        <div className="admin-content">
+            <Switch>
+                <Route path="/admin/products">
+                    <Products/>
+                </Route>
+                <Route path="/admin/categories">
+                    <Products/>
+                </Route>
+                <Route path="/admin/users">
+                    <Products/>
+                </Route>
+            </Switch>
+        </div>
     </div>
 );
 
