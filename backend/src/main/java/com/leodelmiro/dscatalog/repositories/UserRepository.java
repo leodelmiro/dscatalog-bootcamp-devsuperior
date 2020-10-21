32c99586ec5d7a8e0644723d8b55714d9a2182d1
Leonardo Delmiro <leodelmiroms@gmail.com>
2020-10-21 16:43:40 -0300

    User CRUD

---
+++ backend/src/main/java/com/leodelmiro/dscatalog/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.leodelmiro.dscatalog.repositories;
+
+import com.leodelmiro.dscatalog.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
