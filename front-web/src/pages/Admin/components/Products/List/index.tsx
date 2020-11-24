7d1e48a1b55908184c9a27ebf01d695440c4b739
Leonardo Delmiro <leodelmiroms@gmail.com>
2020-11-24 18:27:20 -0300

    Remove product

--- front-web/src/pages/Admin/components/Products/List/index.tsx
+++ front-web/src/pages/Admin/components/Products/List/index.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { ProductsResponse } from 'core/types/Product';
-import { makeRequest } from 'core/utils/request';
+import { makePrivateRequest, makeRequest } from 'core/utils/request';
 import { useHistory } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import Card from '../Card'
 import Pagination from 'core/components/Pagination';
 import CardLoader from '../Loaders/ProductCardLoader';
-
 
 const List = () => {    
     const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
@@ -13,7 +13,8 @@ const List = () => {
     const [activePage, setActivePage] = useState(0);
     const history = useHistory();
 
-    useEffect( () => {
+
+    const getProducts = useCallback(() => {
         const params = {
             page: activePage,
             linesPerPage: 4,
@@ -26,11 +27,31 @@ const List = () => {
             .then(response => setProductsResponse(response.data))
                 .finally(() => {
                     setIsLoading(false);
-            });
+        });
     }, [activePage]);
+
+    useEffect( () => {
+        getProducts();
+    }, [getProducts]);
 
     const handleCreate = () => {
         history.push('/admin/products/create')
+    }
+
+
+    const onRemove = (productId: number) => {
+        const confirm = window.confirm('Deseja realmente excluir este produto?');
+
+        if (confirm) {         
+            makePrivateRequest({url: `/products/${productId}`, method: 'DELETE'})
+                .then(() => {
+                    toast.info('Produto removido com sucesso!');
+                    getProducts();
+                })
+                .catch(() => {
+                    toast.error('Erro ao remover produto!');
+                });
+        }
     }
 
     return (
@@ -41,7 +62,7 @@ const List = () => {
             <div className="admin-list-container">
                 {isLoading ? <CardLoader/> :  (
                     productsResponse?.content.map(product => (
-                        <Card product={product} key={product.id}/>
+                        <Card product={product} key={product.id} onRemove={onRemove}/>
                     ))
                 )}
                
