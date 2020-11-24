718c4682e8abf5b4d4108db304c50ca0f5bddb9c
Leonardo Delmiro <leodelmiroms@gmail.com>
2020-11-23 21:19:59 -0300

    Product Service Integration Test

---
+++ backend/src/test/java/com/leodelmiro/dscatalog/tests/integration/ProductServiceIT.java
@@ -0,0 +1,83 @@
+package com.leodelmiro.dscatalog.tests.integration;
+
+import com.leodelmiro.dscatalog.dto.ProductDTO;
+import com.leodelmiro.dscatalog.services.ProductService;
+import com.leodelmiro.dscatalog.services.exceptions.ResourceNotFoundException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+public class ProductServiceIT {
+
+    @Autowired
+    private ProductService productService;
+
+    private long existingId;
+    private long nonExistingId;
+    private Long countTotalProducts;
+    private Long countPcGamerProducts;
+    private PageRequest pageRequest;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        existingId = 1L;
+        nonExistingId = 1000L;
+        countTotalProducts = 25L;
+        countPcGamerProducts = 21L;
+        pageRequest = PageRequest.of(0, 10);
+    }
+
+    @Test
+    public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
+
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            productService.delete(nonExistingId);
+        });
+
+    }
+
+    @Test
+    public void deleteShouldDoNothingWhenIdExists() {
+        Assertions.assertDoesNotThrow(() -> {
+            productService.delete(existingId);
+        });
+    }
+
+    @Test
+    public void findAllPagedShouldReturnNothingWhenNameDoesNotExist() {
+        String name = "Camera";
+
+        Page<ProductDTO> result = productService.findAllPaged(0L, name, pageRequest);
+
+        Assertions.assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void findAllPagedShouldReturnAllProductsWhenNameIsEmpty() {
+
+        String name = "";
+
+        Page<ProductDTO> result = productService.findAllPaged(0L, name, pageRequest);
+
+        Assertions.assertFalse(result.isEmpty());
+        Assertions.assertEquals(countTotalProducts, result.getTotalElements());
+    }
+
+    @Test
+    public void findAllPagedShouldReturnProductsWhenNameExistsIgnoringCase() {
+
+        String name = "pc gAMeR";
+
+        Page<ProductDTO> result = productService.findAllPaged(0L, name, pageRequest);
+
+        Assertions.assertFalse(result.isEmpty());
+        Assertions.assertEquals(countPcGamerProducts, result.getTotalElements());
+    }
+}
