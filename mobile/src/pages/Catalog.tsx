ee75efb8cf6fd23019cae17347b35dabc6c49a76
Leonardo Delmiro <leodelmiroms@gmail.com>
2020-12-14 21:38:09 -0300

    Conecting to Api

--- mobile/src/pages/Catalog.tsx
+++ mobile/src/pages/Catalog.tsx
@@ -1,39 +1,27 @@
-import React, {useState} from 'react';
-import { ScrollView } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { ScrollView, ActivityIndicator } from 'react-native';
 import { ProductCard, SearchInput} from "../components"
-import productImg from '../assets/product.png'
+import { api } from '../services';
 import { theme } from '../styles';
-
-const products = [
-  {
-    id: 1,
-    imgUrl: productImg,
-    name: "Computador Desktop - Intel Core i7",
-    price: 2279.0
-  },
-  {
-    id: 2,
-    imgUrl: productImg,
-    name: "Computador Desktop - Intel Core i7",
-    price: 2279.0
-  },
-  {
-    id: 3,
-    imgUrl: productImg,
-    name: "Computador Desktop - Intel Core i7",
-    price: 2279.0
-  },
-  {
-    id: 4,
-    imgUrl: productImg,
-    name: "Computador Desktop - Intel Core i7",
-    price: 2279.0
-  },
-]
-
 
 const Catalog: React.FC = () => {
   const [search, setSearch] = useState("");
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  async function fillProducts() {
+    setLoading(true);
+    const res = await api.get(
+      `/products?page=0&linesPerPage=12&direction=ASC&orderBy=name`
+    );
+
+    setProducts(res.data.content);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fillProducts()
+  }, []);
 
   const data = search.length > 0 
     ? products.filter(product => 
@@ -48,10 +36,13 @@ const Catalog: React.FC = () => {
         search={search} 
         setSearch={setSearch}
       />
-      {
-        data.map(product => (
-          <ProductCard {...product} />
-        ))
+      { loading ? (
+        <ActivityIndicator size="large"/>
+        ) :
+      (data.map(product => (
+          <ProductCard {...product} 
+          key={product.id}/>
+      )))
       }
     </ScrollView>
   );
