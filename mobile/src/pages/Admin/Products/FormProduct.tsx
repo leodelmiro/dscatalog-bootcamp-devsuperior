a0ce436a4beb1c46fe6ac6ca758544bec9e0183b
Leonardo Delmiro <leodelmiroms@gmail.com>
2020-12-19 23:15:12 -0300

    Add product

--- mobile/src/pages/Admin/Products/FormProduct.tsx
+++ mobile/src/pages/Admin/Products/FormProduct.tsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
-import { View, Text, ScrollView, TouchableOpacity, Image, Modal, TextInput, ActivityIndicator } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import Toast from 'react-native-tiny-toast';
+import { View, Text, ScrollView, TouchableOpacity, Image, Modal, TextInput, ActivityIndicator, Alert } from 'react-native';
 import { colors, theme, text } from '../../../styles';
 import arrow from '../../../assets/leftArrow.png'
+import { createProduct, getCategories } from '../../../services';
 
 interface FormProductProps {
     setScreen: Function;
@@ -12,35 +14,57 @@ const FormProduct:React.FC<FormProductProps> = (props) => {
     const [loading, setLoading] = useState(false);
     const [edit, setEdit] = useState(false);
     const [showCategories, setShowCategories] = useState(false);
-    const [categories, setCategories] = useState([
-        {
-            id: 3,
-            name: "Computador"
-        },
-        {
-            id: 2,
-            name: "Eletrônicos"
-        },
-        {
-            id: 1,
-            name: "Notebooks"
-        },
-        {
-            id: 4,
-            name: "Hardwares"
-        },
-        {
-            id: 5,
-            name: "Periféricos"
-        }
-    ])
+    const [categories, setCategories] = useState([])
     const [product, setProduct] = useState({
-        name: null,
-        description: null,
-        image: null,
-        price: null,
-        categories: null
+        name: "",
+        description: "",
+        image: "",
+        price: 0,
+        categories: []
     });
+
+    function handleSave() {
+        !edit && newProduct();
+    }
+
+    async function newProduct() {
+        setLoading(true);
+        const cat = replaceCategory();
+        const data = {
+            ...product,
+            categories: [
+                {
+                    id: cat
+                }
+            ]
+        };
+
+        try {
+            await createProduct(data);
+            Toast.showSuccess("Produto criado com sucesso");
+        } catch (res) {
+            Toast.show("Erro ao salvar ...")
+        }
+        
+        setLoading(false);
+    };
+
+    function replaceCategory() {
+        const cat = categories.find( category => category.name === product.categories);
+
+        return cat.id;
+    }
+
+    async function loadCategories() {
+        setLoading(true);
+        const res = await getCategories();
+        setCategories(res.data.content);
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        loadCategories();
+    }, []);
 
     return (
         <View style={theme.formContainer}>
@@ -76,30 +100,62 @@ const FormProduct:React.FC<FormProductProps> = (props) => {
                                 <Image source={arrow} />
                                 <Text style={text.goBackText}>Voltar</Text>
                             </TouchableOpacity>
-                            <TextInput placeholder="Nome do produto" style={theme.formInput}/>
+                            <TextInput 
+                                placeholder="Nome do produto" 
+                                style={theme.formInput}
+                                value={product.name}
+                                onChangeText={(e) => setProduct({...product, name: e})}
+                            />
                             <TouchableOpacity 
                                 onPress={() => setShowCategories(!showCategories)} 
                                 style={theme.selectInput}
                             >
-                                <Text style={[product.categories === null && {color: colors.mediumGray}, product.categories !== null && {color: colors.black}]}>
-                                    {product.categories === null
+                                <Text style={[product.categories.length === 0 && {color: colors.mediumGray}, product.categories.length !== 0 && {color: colors.black}]}>
+                                    {product.categories.length === 0 
                                         ? "Escolha uma categoria"
                                         : product.categories}
                                 </Text>
                             </TouchableOpacity>
-                            <TextInput placeholder="Preço" style={theme.formInput}/>
+                            <TextInput 
+                                placeholder="Preço" 
+                                style={theme.formInput}
+                                value={product.price}
+                                onChangeText={(e) => setProduct({...product, price: parseInt(e) })}
+                            />
                             <TouchableOpacity activeOpacity={0.8} style={theme.uploadBtn}>
                                 <Text style={text.uploadText}>Carregar imagem</Text>
                             </TouchableOpacity>
                             <Text style={text.fileSize}>
                                 As imagens devem ser JPG ou PNG e não devem ultrapassar 5 mb.
                             </Text>
-                            <TextInput multiline placeholder="Descrição" style={theme.textArea}/>
+                            <TextInput 
+                                multiline 
+                                placeholder="Descrição" 
+                                style={theme.textArea}
+                                value={product.description}
+                                onChangeText={(e) => setProduct({...product, description: e})}
+                            />
                             <View style={theme.buttonContainer}>
-                                <TouchableOpacity style={theme.deleteBtn}>
+                                <TouchableOpacity style={theme.deleteBtn} onPress={() => {
+                                    Alert.alert(
+                                        "Deseja cancelar?",
+                                        "Os dados inseridos não serão salvos",
+                                        [
+                                            {
+                                                text: "Voltar",
+                                                style: "cancel",
+                                            },
+                                            {
+                                                text: "Confirmar",
+                                                onPress: () => setScreen("products"),
+                                                style: "default",
+                                            }
+                                        ]
+                                    )
+                                }}>
                                     <Text style={text.deleteText}>Cancelar</Text>
                                 </TouchableOpacity>
-                                <TouchableOpacity style={theme.saveBtn}>
+                                <TouchableOpacity style={theme.saveBtn} onPress={() => handleSave()}>
                                     <Text style={text.saveText}>Salvar</Text>
                                 </TouchableOpacity>
                             </View>
--- mobile/src/services/auth.ts
+++ mobile/src/services/auth.ts
@@ -48,3 +48,8 @@ export async function doLogout() {
         console.warn(e);
     }
 }
+
+export async function userToken() {
+    const token = await AsyncStorage.getItem("@token");
+    return token;
+}
